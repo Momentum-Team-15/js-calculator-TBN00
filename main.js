8ddbd47c8a9f5ee2e6722923c0e85f8fb0373825
Taylor <tbnelson01@gmail.com>
2022-09-20 19:02:57 -0400

    Calculator

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-let squares = document.querySelectorAll(".square");
 
-let frame = document.querySelector("#input");
 
+const display = document.querySelector("#display")
+
+let squares = document.querySelectorAll(".square")
 for (let square of squares) {
-    square.addEventListener("click", (event) => {
+    square.addEventListener("mousedown", (event) => {
         square.classList.remove("blue");
         square.classList.add("pressed");
-
     });
+    square.addEventListener("mouseup", (event) => {
+        square.classList.add("blue");
+        square.classList.remove("pressed");
+    });
+
+    square.addEventListener("click", (event) => {
+        console.log(square.innerText)
+
+        let newElement = document.createElement('div')
+        let text = document.createTextNode(square.innerText)
+        newElement.appendChild(text)
+        display.appendChild(newElement)
+    })
+
 }
-
-
