018ecd8b721e456c38af68feef45ed1a745ad109
Taylor <tbnelson01@gmail.com>
2022-09-21 09:19:33 -0400

    Calculator

--- main.js
+++ main.js
@@ -25,11 +25,27 @@ for (let square of squares) {
 }
 
 let equals = document.querySelector("#equal")
-    equals.addEventListener("click", (event) => {
-        display.innerText = eval(display.innerText);
+equals.addEventListener("mousedown", (event) => {
+    equals.classList.remove("blue");
+    equals.classList.add("pressed");
+});
+equals.addEventListener("mouseup", (event) => {
+    equals.classList.add("blue");
+    equals.classList.remove("pressed");
+});
+equals.addEventListener("click", (event) => {
+    display.innerText = eval(display.innerText);
 });
 
 let clear = document.querySelector("#clear")
-    clear.addEventListener("click", (event) => {
-        display.innerText = "";
-    });
+clear.addEventListener("mousedown", (event) => {
+    clear.classList.remove("blue");
+    clear.classList.add("pressed");
+});
+clear.addEventListener("mouseup", (event) => {
+    clear.classList.add("blue");
+    clear.classList.remove("pressed");
+});
+clear.addEventListener("click", (event) => {
+    display.innerText = "";
+});
